98a648658a474d3de115357bbbcd96b4e4296cb4
David Crammer <dcrammer777@gmail.com>
2023-08-22 16:24:14 -0700

    Test 2

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import format from "./format"
-import verify from "./verify"
-import useGet from "./useGet"
-import useTrigger from "./useTrigger"
-
-export { format, verify, useGet, useTrigger }
---
+++ src/index.mjs
@@ -0,0 +1,6 @@
+import format from "./format.mjs"
+import verify from "./verify.mjs"
+import useGet from "./useGet.mjs"
+import useTrigger from "./useTrigger.mjs"
+
+export { format, verify, useGet, useTrigger }
